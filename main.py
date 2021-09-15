d6d7629d9cab3083d51fa8147a21f69237f71454
Maxim Shevtsov <xmaksutx@gmail.com>
2021-09-15 17:27:57 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+#Simple Stock Price App
+
+Shown are the stock closing price and volume of Google!
+
+""")
+
+# define the ticker symbol
+tickerSymbol = 'GOOGL'
+# get data on this ticker
+tikerData = yf.Ticker(tickerSymbol)
+# get the histirical prices for this ticker
+tickerDf = tikerData.history(periad='id', start='2010-5-31', end='2021-8-30')
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
