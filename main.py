bbe4ebf29a588b08595e64fd9d3bb3bf33dcdefc
Maxim Shevtsov <xmaksutx@gmail.com>
2021-09-15 17:34:25 +0300

    com

--- main.py
+++ main.py
@@ -16,5 +16,11 @@ tikerData = yf.Ticker(tickerSymbol)
 # get the histirical prices for this ticker
 tickerDf = tikerData.history(periad='id', start='2010-5-31', end='2021-8-30')
 
+st.write('''
+## Closing Price
+''')
 st.line_chart(tickerDf.Close)
+st.write('''
+## Volume Price
+''')
 st.line_chart(tickerDf.Volume)
